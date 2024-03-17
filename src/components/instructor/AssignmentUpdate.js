c2156c2fe5f86b6989f1b751a9eb68a392abd196
Kyle Absten <kabsten@csumb.edu>
2024-03-17 12:59:07 -0700

    Assignment View and Assignment update working. Before linking backend

--- src/components/instructor/AssignmentUpdate.js
+++ src/components/instructor/AssignmentUpdate.js
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 //  instructor updates assignment title, dueDate 
 //  use an mui Dialog
@@ -6,11 +12,43 @@ import React, { useState } from 'react';
 
 const AssignmentUpdate = (props)  => {
 
+    const [isOpen, setOpen] = useState(false);
+    const [message, setMessage] = useState('');
+    const [assignment, setAssignment] = useState({assignmentId: '', title: '', dueDate: ''});
+
+    const editOpen = () => {
+        setOpen(true);
+        setMessage('');
+        setAssignment(props.assignment);
+    }
+    const editChange = (event) => {
+        setAssignment({...assignment,[event.target.name]:event.target.value})
+    }
+    const editClose = () => {
+        setOpen(false);
+    }
+    const onSave = () => {
+        props.save(assignment);
+        setOpen(false);
+    }
 
     return (
         <>
-          <h3>Not implemented</h3>  
-        </>                       
+            <Button onClick={editOpen}>Edit</Button>
+            <Dialog open={isOpen}>
+                <DialogTitle>Edit Assignment</DialogTitle>
+                <DialogContent style={{paddingTop:20}}>
+                    <h4>{message}</h4>
+                    <TextField style={{padding:10}} fullWidth label='assignmentId' name='assignmentId' value={assignment.assignmentId} InputProps={{readOnly: true,}} />
+                    <TextField style={{padding:10}} autoFocus fullWidth label='title' name='title' value={assignment.title} onChange={editChange} />
+                    <TextField style={{padding:10}} fullWidth label='dueDate' name='dueDate' value={assignment.dueDate} onChange={editChange} />
+                </DialogContent>
+                <DialogActions>
+                    <Button color='secondary' onClick={editClose}>Close</Button>
+                    <Button color='primary' onClick={onSave}>Save</Button>
+                </DialogActions>
+            </Dialog>
+        </>
     )
 }
 
--- src/components/instructor/AssignmentsView.js
+++ src/components/instructor/AssignmentsView.js
@@ -1,5 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import {useLocation} from 'react-router-dom'
+import Button from "@mui/material/Button";
+import {useLocation} from 'react-router-dom';
+import 'react-confirm-alert/src/react-confirm-alert.css'
+import {confirmAlert} from "react-confirm-alert";
+import AssignmentUpdate from "./AssignmentUpdate";
 
 // instructor views assignments for their section
 // use location to get the section value 
@@ -11,12 +15,66 @@ import {useLocation} from 'react-router-dom'
 
 const AssignmentsView = (props) => {
 
-    const location = useLocation();
-    const {secNo, courseId, secId} = location.state;
+    //const location = useLocation();
+    //const {secNo, courseId, secId} = location.state;
+    const [assignments, setAssignments] = useState([
+        {assignmentId: '9' , title: 'Assignment 1', dueDate: '2023-5-08'},
+        {assignmentId: '10' , title: 'Assignment 2', dueDate: '2023-5-09'},
+        {assignmentId: '1' , title: 'Assignment 3', dueDate: '2023-5-10',}]
+    );
+    const headers = ['Assignment ID', 'Title', 'Due Date', '', ''];
+    const [message, setMessage] = useState('');
+
+    const deleteAlert = (event) => {
+        confirmAlert({
+            title: 'Confirm to delete',
+            message: 'Do you really want to delete',
+            buttons:[
+                {
+                    label: 'Yes',
+                    onClick: () => doDelete(event)
+                },
+        {
+            label: 'No',
+        }
+            ]
+        })
+    }
+
+    const doDelete= (event) => {
+        const row_index =event.target.parentNode.parentNode.rowIndex -1;
+        const assignmentsCopy = assignments.filter((a, idx) => idx!==row_index);
+        setAssignments(assignmentsCopy);
+    }
+
+    const onSave= (assignment) => {
+        const assignmentsCopy = assignments.map((a) =>(a.assignmentId===assignment.assignmentId) ? assignment : a);
+        setAssignments(assignmentsCopy);
+    }
      
     return(
-        <> 
-           <h3>Not implemented</h3>
+        <>
+            <h3>Assignments</h3>
+            <h4>{message}</h4>
+            <table className = 'Center'>
+                <thead>
+                <tr>
+                    {headers.map((h,idx) => <th key={idx}>{h} </th>)}
+                </tr>
+                </thead>
+                <tbody>
+                {assignments.map((a) =>
+                    <tr key={a.assignmentId}>
+                        <td>{a.assignmentId}</td>
+                        <td>{a.title}</td>
+                        <td>{a.dueDate}</td>
+                        <td><AssignmentUpdate assignment={a} save={onSave} /></td>
+                        <td><Button onClick={deleteAlert}>Delete</Button></td>
+                    </tr>
+                )}
+                </tbody>
+            </table>
+            Add an assignment
         </>
     );
 }
