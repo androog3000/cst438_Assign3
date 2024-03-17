338fd7db7c2816aefbeaaa032fff02b84b2935a9
Kyle Absten <kabsten@csumb.edu>
2024-03-17 15:28:46 -0700

    AssignmentsView mostly done.

--- src/components/instructor/AssignmentsView.js
+++ src/components/instructor/AssignmentsView.js
@@ -4,6 +4,8 @@ import {useLocation} from 'react-router-dom';
 import 'react-confirm-alert/src/react-confirm-alert.css'
 import {confirmAlert} from "react-confirm-alert";
 import AssignmentUpdate from "./AssignmentUpdate";
+import {SERVER_URL} from "../../Constants";
+
 
 // instructor views assignments for their section
 // use location to get the section value 
@@ -15,15 +17,35 @@ import AssignmentUpdate from "./AssignmentUpdate";
 
 const AssignmentsView = (props) => {
 
-    //const location = useLocation();
-    //const {secNo, courseId, secId} = location.state;
+    const location = useLocation();
+    const {secNo, courseId, secId} = location.state;
+    /*
     const [assignments, setAssignments] = useState([
         {assignmentId: '9' , title: 'Assignment 1', dueDate: '2023-5-08'},
         {assignmentId: '10' , title: 'Assignment 2', dueDate: '2023-5-09'},
         {assignmentId: '1' , title: 'Assignment 3', dueDate: '2023-5-10',}]
-    );
+    );*/
+    const [assignments, setAssignments] = useState([]);
     const headers = ['Assignment ID', 'Title', 'Due Date', '', ''];
     const [message, setMessage] = useState('');
+    const fetchAssignments = async () =>{
+        try{
+            const response = await fetch(`${SERVER_URL}/sections/8/assignments`);
+            if (response.ok){
+                const assignments = await response.json()
+                setAssignments(assignments);
+            } else {
+                const json = await response.json();
+                setMessage('Response error: '+json);
+            }
+        } catch(err) {
+            setMessage('Network Error: ' +err);
+        }
+    }
+
+    useEffect( () =>{
+        fetchAssignments();
+    }, []);
 
     const deleteAlert = (event) => {
         confirmAlert({
@@ -43,13 +65,56 @@ const AssignmentsView = (props) => {
 
     const doDelete= (event) => {
         const row_index =event.target.parentNode.parentNode.rowIndex -1;
-        const assignmentsCopy = assignments.filter((a, idx) => idx!==row_index);
-        setAssignments(assignmentsCopy);
+        deleteAssignment(assignments[row_index].id);
+
     }
 
-    const onSave= (assignment) => {
+    const deleteAssignment = async (assignmentId) =>{
+        try {
+            const response = await fetch (`${SERVER_URL}/assignments/${assignmentId}`,
+                {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                });
+            if(response.ok) {
+                setMessage('Assignment deleted');
+                fetchAssignments();
+            } else {
+                const rc = await response.json();
+                setMessage('Deleted failed: ' + rc.message);
+            }
+        } catch (err) {
+            setMessage('Network error: ' + err);
+        }
+    }
+
+    /*const onSave= (assignment) => {
         const assignmentsCopy = assignments.map((a) =>(a.assignmentId===assignment.assignmentId) ? assignment : a);
         setAssignments(assignmentsCopy);
+    }*/
+
+    const onSave = async (assignment) =>{
+        try {
+            const response = await fetch(`${SERVER_URL}/assignments`,
+                {
+                    method: 'PUT',
+                    headers: {
+                        'Content-type': 'application/json',
+                    },
+                    body: JSON.stringify(assignment),
+                });
+            if (response.ok){
+                setMessage('Assignment added');
+                fetchAssignments();
+            } else {
+                const rc = await response.json();
+                setMessage('Save error: ' + rc.message);
+            }
+        } catch(err) {
+            setMessage('Network error: ' + err);
+        }
     }
      
     return(
@@ -64,8 +129,8 @@ const AssignmentsView = (props) => {
                 </thead>
                 <tbody>
                 {assignments.map((a) =>
-                    <tr key={a.assignmentId}>
-                        <td>{a.assignmentId}</td>
+                    <tr key={a.id}>
+                        <td>{a.id}</td>
                         <td>{a.title}</td>
                         <td>{a.dueDate}</td>
                         <td><AssignmentUpdate assignment={a} save={onSave} /></td>
