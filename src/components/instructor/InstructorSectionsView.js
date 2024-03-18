236529f5e85347c8ef11118b8b267c32037529be
Juan Gutierrez <juangutierrez2@csumb.edu>
2024-03-17 23:14:07 -0700

    Fixed URL call to include properly sourced year, semester data. Added first call to fetchSection()

--- src/components/instructor/InstructorSectionsView.js
+++ src/components/instructor/InstructorSectionsView.js
@@ -1,23 +1,78 @@
 import React, {useState, useEffect} from 'react';
+import SectionUpdate from "../admin/SectionUpdate";
+import Button from "@mui/material/Button";
+import SectionAdd from "../admin/SectionAdd";
+import {SERVER_URL} from "../../Constants";
+import {Link, useLocation} from "react-router-dom";
 
-// instructor views a list of sections they are teaching 
+// instructor views a list of sections they are teaching
 // use the URL /sections?email=dwisneski@csumb.edu&year= &semester=
 // the email= will be removed in assignment 7 login security
 // The REST api returns a list of SectionDTO objects
 // The table of sections contains columns
 //   section no, course id, section id, building, room, times and links to assignments and enrollments
-// hint:  
+// hint:
 // <Link to="/enrollments" state={section}>View Enrollments</Link>
 // <Link to="/assignments" state={section}>View Assignments</Link>
 
 const InstructorSectionsView = (props) => {
 
-    
-     
+    const headers = ['SecNo', 'CourseId', 'SecId', 'Building', 'Room', 'Times', '', ''];
+
+    const [sections, setSections] = useState([]);
+
+    const [message, setMessage] = useState('');
+
+    const location = useLocation();
+    const fetchSections = async () => {
+        try {
+            const {year, semester} = location.state;
+            const response = await fetch(`${SERVER_URL}/sections?email=dwisneski@csumb.edu&year=${year}&semester=${semester}`);
+            if (response.ok) {
+                const data = await response.json();
+                setSections(data);
+            } else {
+                const rc = await response.json();
+                setMessage(rc.message);
+            }
+        } catch(err) {
+            setMessage("network error: "+err);
+        }
+    }
+
+    useEffect(() => {
+        fetchSections();
+    }, []);
+
     return(
-        <> 
-           <h3>Not implemented</h3>
-        </>
+        <div>
+            <h3>Sections</h3>
+
+            <h4>{message}</h4>
+            <h4>Your current sections</h4>
+            <table className="Center" >
+                <thead>
+                <tr>
+                    {headers.map((s, idx) => (<th key={idx}>{s}</th>))}
+                </tr>
+                </thead>
+                <tbody>
+                {sections.map((s) => (
+                    <tr key={s.secNo}>
+                        <td>{s.secNo}</td>
+                        <td>{s.courseId}</td>
+                        <td>{s.secId}</td>
+                        <td>{s.building}</td>
+                        <td>{s.room}</td>
+                        <td>{s.times}</td>
+                        <td><Link to="/enrollments" state={s}>View Enrollments</Link></td>
+                        <td><Link to="/assignments" state={s}>View Assignments</Link></td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            <SectionAdd  onClose={fetchSections} />
+        </div>
     );
 }
 
