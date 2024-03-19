ae0d34e4ef8521046c5839c56699161802f2a97e
kd65541 <45408593+kd65541@users.noreply.github.com>
2024-03-19 01:38:14 -0700

    solved conflict messages on AssignmentUpdate.js

--- src/components/instructor/AssignmentUpdate.js
+++ src/components/instructor/AssignmentUpdate.js
@@ -1,12 +1,4 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogTitle from "@mui/material/DialogTitle";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-=======
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
@@ -14,7 +6,6 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { SERVER_URL } from '../../Constants';
->>>>>>> fb62db22fbe094540200ca828fb879a1f069c1bb
 
 const AssignmentUpdate = (props) => {
     const [open, setOpen] = useState(false);
@@ -45,47 +36,6 @@ const AssignmentUpdate = (props) => {
         setAssignment(props.assignment);
     };
 
-<<<<<<< HEAD
-    const [isOpen, setOpen] = useState(false);
-    const [message, setMessage] = useState('');
-    const [assignment, setAssignment] = useState({assignmentId: '', title: '', dueDate: ''});
-
-    const editOpen = () => {
-        setOpen(true);
-        setMessage('');
-        setAssignment(props.assignment);
-    }
-    const editChange = (event) => {
-        setAssignment({...assignment,[event.target.name]:event.target.value})
-    }
-    const editClose = () => {
-        setOpen(false);
-    }
-    const onSave = () => {
-        props.save(assignment);
-        setOpen(false);
-    }
-
-    return (
-        <>
-            <Button onClick={editOpen}>Edit</Button>
-            <Dialog open={isOpen}>
-                <DialogTitle>Edit Assignment</DialogTitle>
-                <DialogContent style={{paddingTop:20}}>
-                    <h4>{message}</h4>
-                    <TextField style={{padding:10}} fullWidth label='assignmentId' name='assignmentId' value={assignment.assignmentId} InputProps={{readOnly: true,}} />
-                    <TextField style={{padding:10}} autoFocus fullWidth label='title' name='title' value={assignment.title} onChange={editChange} />
-                    <TextField style={{padding:10}} fullWidth label='dueDate' name='dueDate' value={assignment.dueDate} onChange={editChange} />
-                </DialogContent>
-                <DialogActions>
-                    <Button color='secondary' onClick={editClose}>Close</Button>
-                    <Button color='primary' onClick={onSave}>Save</Button>
-                </DialogActions>
-            </Dialog>
-        </>
-    )
-}
-=======
     const editClose = () => {
         setOpen(false);
         props.onClose();
@@ -188,6 +138,5 @@ const AssignmentUpdate = (props) => {
         </div>
     );
 };
->>>>>>> fb62db22fbe094540200ca828fb879a1f069c1bb
 
 export default AssignmentUpdate;
